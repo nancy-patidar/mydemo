210668700535d95032cc96fbee08c0473648848e
nain <nancypatidar75@gmail.com>
2025-08-03 21:00:32 +0530

    triangle pattern java program added

---
+++ trianglepattern.java
@@ -0,0 +1,14 @@
+public class trianglepattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 0; i <= rows; i++) {
+           
+            for (int j =1; j <=i; j++) {
+System.out.println("*");
+       
+            }
+        
+        }System.out.println(" ");
+    }
+}
